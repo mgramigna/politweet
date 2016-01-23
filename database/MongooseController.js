a3bf492545271fa12e0d77a5b29d2465a01ab248
Matthew Gramigna <mgramigna@umass.edu>
2016-01-23 02:40:47 -0500

    Formatting

--- database/MongooseController.js
+++ database/MongooseController.js
@@ -6,33 +6,39 @@ var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log('connected to tweets database');
+    console.log('connected to tweets database');
 });
 
 module.exports.saveTweet = function(tweet) {
-	tweet.save(function(err, tweet) {
-		if(err) return console.error(err);
-		console.log('Tweet saved to db');
-	});
+    tweet.save(function(err, tweet) {
+        if (err) return console.error(err);
+        console.log('Tweet saved to db');
+    });
 };
 
 module.exports.getTweetsByCandidate = function(candidate, num, callback) {
-	Tweet.find({'candidate': candidate}).limit(num).exec(function(err, tweets) {
-		if(err) return console.error(err);
-		callback(tweets);
-	});
+    Tweet.find({
+        'candidate': candidate
+    }).limit(num).exec(function(err, tweets) {
+        if (err) return console.error(err);
+        callback(tweets);
+    });
 };
 
 module.exports.getTweetsByParty = function(party, num, callback) {
-	Tweet.find({'party': party}).limit(num).exec(function(err, tweets) {
-		if(err) return console.error(err);
-		callback(tweets);
-	});
+    Tweet.find({
+        'party': party
+    }).limit(num).exec(function(err, tweets) {
+        if (err) return console.error(err);
+        callback(tweets);
+    });
 };
 
 module.exports.deleteTweet = function(tweetId) {
-	Tweet.remove({'tweet.id': tweetId}, function(err) {
-		if(err) return console.error(err);
-		console.log('Tweet deleted from db');
-	});
+    Tweet.remove({
+        'tweet.id': tweetId
+    }, function(err) {
+        if (err) return console.error(err);
+        console.log('Tweet deleted from db');
+    });
 };
