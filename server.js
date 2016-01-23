4896f00acb62c71f7b9e2af9f40c71d7cd4b6f8e
Justin Kennedy <jekennedy@umass.edu>
2016-01-23 02:32:51 -0500

    Async database calls on GET

--- server.js
+++ server.js
@@ -2,23 +2,38 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+async = require('async');
 
 var twitter = require('./twitter/Twitter');
 var db = require('./database/MongooseController');
 var Tweet = require('./database/schemas/tweet');
 var candidates = [];
 
+//Serve Client
 app.use(express.static(__dirname + '/public'));
 
+//Initial Database Query on GET
+var asyncObject = {};
+candidates.forEach(function(candidate){
+  asyncObject[candidate.name] = function(callback){
+    db.getTweetsByCandidate(candidate.name, 1, function(tweets){
+      callback(null, tweets);
+    });
+  };
+});
 app.get('/init', function(req, res){
-  //Async.series stuff
+  async.series(asyncObject, function(err, response){
+    res.json(response);
+  });
 });
 
+//Socket Connections
 var users = [];
 io.on('connection', function(socket){
   users.push(socket);
 });
 
+//Tweet Handler
 twitter.onTweet(function(tweet){
   db.saveTweet(new Tweet(tweet));
   users.forEach(function(user){
