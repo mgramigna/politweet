b844edf1410ca5525608a7fdc69c940e6e51f198
Justin Kennedy <jekennedy@umass.edu>
2016-01-23 00:49:56 -0500

    Socket stuff in server.js

--- server.js
+++ server.js
@@ -2,17 +2,28 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/tweets');
-var db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('connected to tweets database');
-});
+// var twitter = require('./twitter/Twitter');
+// var db = require('./database/MongooseController');
+var candidates = [];
 
 app.use(express.static(__dirname + '/public'));
+
+app.get('/init', function(req, res){
+  //Async.series stuff
+});
+
+var users = [];
+io.on('connection', function(socket){
+  users.push(socket);
+});
+
+// twitter.onTweet(function(tweet){
+//   db.saveTweet(tweet);
+//   users.forEach(function(user){
+//     user.volatile.emit('tweet', tweet);
+//   });
+// });
 
 server.listen(3000, function() {
   console.log("Server started, listening on port 3000");
