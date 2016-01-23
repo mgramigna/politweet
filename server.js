921a4c14ac5675991174eaf18853346b34d0c8cf
Andrew Bass <arbass@umass.edu>
2016-01-22 21:59:55 -0500

    Basic server

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+app.use(express.static(__dirname + '/public'));
+
+server.listen(3000, function() {
+  console.log("Server started, listening on port 3000");
+});
