bb932aecf8277b621e9b65ca95ceb24d28a0c054
Andrew Bass <arbass@umass.edu>
2016-01-23 23:45:03 -0500

    Switch between graphs

--- public/d3/sentimentDataScript.js
+++ public/d3/sentimentDataScript.js
@@ -3,41 +3,48 @@ var bernieData = ['Sanders'];
 var trumpData = ['Trump'];
 var rubioData = ['Rubio'];
 
-$('.sentimentBtn').on('click', function(evt) {
-  ReactDOM.unmountComponentAtNode(document.getElementById('container'));
-
-  $.ajax('/sentiments', {
-   success: function(data) {
-     data.forEach(function(obj){
-       clintonData.push(obj.data.candidates['hillary clinton']*100);
-       bernieData.push(obj.data.candidates['bernie sanders']*100);
-       trumpData.push(obj.data.candidates['donald trump']*100);
-       rubioData.push(obj.data.candidates['marco rubio']*100);
-     });
-     var chart = c3.generate({
-      bindto: '#container',
-      data: {
-        x: 'x',
-        columns: [
-          ['x', 5, 10, 15, 20, 25, 30],
-          clintonData,
-          bernieData,
-          trumpData,
-          rubioData
-        ]
+$.ajax('/sentiments', {
+ success: function(data) {
+   data.forEach(function(obj){
+     clintonData.push(obj.data.candidates['hillary clinton']*100);
+     bernieData.push(obj.data.candidates['bernie sanders']*100);
+     trumpData.push(obj.data.candidates['donald trump']*100);
+     rubioData.push(obj.data.candidates['marco rubio']*100);
+   });
+   window.chart = c3.generate({
+    bindto: '#graphContainer',
+    data: {
+      x: 'x',
+      columns: [
+        ['x', 5, 10, 15, 20, 25, 30],
+        clintonData,
+        bernieData,
+        trumpData,
+        rubioData
+      ]
+    },
+    axis: {
+      x: {
+        label: 'Time Since Last Calculation (Minutes)'
       },
-      axis: {
-        x: {
-          label: 'Time Since Last Calculation (Minutes)'
-        },
-        y: {
-          label: 'Tweet Positivity (%)'
-        }
+      y: {
+        label: 'Tweet Positivity (%)'
       }
-     });
-   },
-   error: function() {
-     console.error('error');
-   }
-  });
+    }
+   });
+ },
+ error: function() {
+   console.error('error');
+ }
+});
+
+$('.chartBtn').on('click', function(evt) {
+  $('#graphContainer').hide();
+  $('#container').show();
+})
+
+$('.sentimentBtn').on('click', function(evt) {
+  $('#container').hide();
+  $('#graphContainer').show();
+  window.chart.resize();
 });
